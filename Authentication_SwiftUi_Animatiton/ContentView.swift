2e293b3450ad12a0af3f8d285eee44d87265907d
Tassio Rocha <tassiorocha@frwk.com.br>
2020-09-09 20:41:55 -0300

    Initial Commit

---
+++ Authentication_SwiftUi_Animatiton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Authentication_SwiftUi_Animatiton
+//
+//  Created by Tássio Rocha on 09/09/20.
+//  Copyright © 2020 Tássio Rocha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Authentication_SwiftUi_AnimatitonTests/Authentication_SwiftUi_AnimatitonTests.swift
@@ -0,0 +1,34 @@
+//
+//  Authentication_SwiftUi_AnimatitonTests.swift
+//  Authentication_SwiftUi_AnimatitonTests
+//
+//  Created by Tássio Rocha on 09/09/20.
+//  Copyright © 2020 Tássio Rocha. All rights reserved.
+//
+
+import XCTest
+@testable import Authentication_SwiftUi_Animatiton
+
+class Authentication_SwiftUi_AnimatitonTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
